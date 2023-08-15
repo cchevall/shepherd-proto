66ca862327ad5c7b1265b9f110542b47d5cb6223
Christophe Chevallay <christophe.chevallay@gmail.com>
2023-08-15 18:57:53 +0200

    add score manager

--- Assets/Scripts/TitleScreen/TitleScreenManager.cs
+++ Assets/Scripts/TitleScreen/TitleScreenManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.IO;
 
 public class TitleScreenManager : MonoBehaviour
 {
@@ -9,9 +10,13 @@ public class TitleScreenManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKey)
+        if (Input.anyKeyDown)
         {
             SceneManager.LoadScene(1);
+
+            //ScoreManager.Instance.AddScore("AAA", Random.Range(0, 42));
+            //ScoreManager.Instance.LoadScores();
+            //Debug.Log(ScoreManager.Instance.GetScores());
         }
     }
 }
