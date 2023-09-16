eef9f7719fd9c6b1b468b193add9dd381152916b
Christophe Chevallay <christophe.chevallay@gmail.com>
2023-09-16 18:04:32 +0200

    Improve Player Controls and Add obstacle prefabs

--- Assets/Scripts/InGame/MoveForward.cs
+++ Assets/Scripts/InGame/MoveForward.cs
@@ -2,9 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum MoveDirection {
+    Forward,
+    Backward,
+    WorldForward,
+    WorldBackward,
+}
+
 public class MoveForward : MonoBehaviour
 {
     [SerializeField] float additionalSpeed = 0f;
+    [SerializeField] MoveDirection moveDirection = MoveDirection.WorldBackward;
 
     // Update is called once per frame
     void Update()
@@ -19,7 +27,24 @@ public class MoveForward : MonoBehaviour
             return;
         }
         float finalSpeed = GameManager.Instance.gameSpeed + additionalSpeed;
-        transform.Translate(Vector3.forward * Time.deltaTime * finalSpeed, Space.Self);
+        switch (moveDirection)
+        {
+            case MoveDirection.WorldBackward:
+                transform.Translate(-Vector3.forward * Time.deltaTime * finalSpeed, Space.World);
+                return ;
+            case MoveDirection.WorldForward:
+                transform.Translate(Vector3.forward * Time.deltaTime * finalSpeed, Space.World);
+                return ;
+            case MoveDirection.Forward:
+                transform.Translate(Vector3.forward * Time.deltaTime * finalSpeed, Space.Self);
+                return ;
+            case MoveDirection.Backward:
+                transform.Translate(-Vector3.forward * Time.deltaTime * finalSpeed, Space.Self);
+                return ;
+            default:
+                transform.Translate(-Vector3.forward * Time.deltaTime * finalSpeed, Space.World);
+                return ;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
