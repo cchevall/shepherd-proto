ffb0b9e05a084fc31566b8f15e0baf8edb0134a0
Christophe Chevallay <christophe.chevallay@gmail.com>
2023-09-17 17:10:59 +0200

    Spawn fog mountains trees and grass

--- Assets/Scripts/InGame/DestroyOffLimit.cs
+++ Assets/Scripts/InGame/DestroyOffLimit.cs
@@ -4,14 +4,12 @@ using UnityEngine;
 
 public class DestroyOffLimit : MonoBehaviour
 {
-    private float minZPos = -50f;
-    private float maxZPos = LevelConfig.offLimitZPos + 100f;
-    // Start is called before the first frame update
+    private float minZPos = -LevelConfig.offLimitZPos * 2f;
+    private float maxZPos = LevelConfig.offLimitZPos * 2f;
 
-    // Update is called once per frame
     void Update()
     {
-        if (transform.position.z > maxZPos || transform.position.z < minZPos)
+        if (transform.position.z > maxZPos || transform.position.z < minZPos || transform.position.y < LevelConfig.offLimitYBottomPos || transform.position.y > LevelConfig.offLimitYTopPos)
         {
             Destroy(gameObject);
         }
